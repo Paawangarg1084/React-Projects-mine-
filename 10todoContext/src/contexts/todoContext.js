681c4115f09edcdcc47b49095e82f6e6f67bf163
Paawan Garg <paawangarg216@gmail.com>
2026-03-06 14:31:22 +0530

    coped gitcode

--- 10todoContext/src/contexts/todoContext.js
+++ 10todoContext/src/contexts/todoContext.js
@@ -1,19 +1,22 @@
-import { createContext, useContext } from "react";
+import {createContext, useContext} from "react"
 
-export const todoContext = createContext({
-    todos: [{
-        id:1,
-        todo: "Todo Msg",
-        completed: false,
-
-    }],addTodo:(todo)  => {},
-        updateTodo: (id,todo) => {},
-        deleteTodo: (id) =>{},
-        toggleComplete: (id) =>{},
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
 })
 
-export const usetodo = ()=>{
-    return useContext(todoContext)
+
+export const useTodo = () => {
+    return useContext(TodoContext)
 }
 
-export const TodoProvider = todoContext.Provider
+export const TodoProvider = TodoContext.Provider
